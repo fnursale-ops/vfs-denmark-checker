c115f94a1ac86d6895d6323cf296b66c66b22133
fnursale-ops <f.nursale@gmail.com>
2026-05-18 10:01:19 +0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -5,53 +5,18 @@ from playwright.async_api import async_playwright
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-EMAIL = os.getenv("VFS_EMAIL")
-PASSWORD = os.getenv("VFS_PASSWORD")
-
-LOGIN_URL = "https://visa.vfsglobal.com/tur/tr/dnk/login"
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
-async def send_message(text):
-    bot.send_message(chat_id=CHAT_ID, text=text)
+async def main():
+    try:
+        bot.send_message(
+            chat_id=CHAT_ID,
+            text="✅ Telegram test mesajı başarılı. Sistem çalışıyor."
+        )
+        print("TEST MESAJI GONDERILDI")
 
-async def check_slots():
-    async with async_playwright() as p:
+    except Exception as e:
+        print(e)
 
-        browser = await p.chromium.launch(headless=True)
-
-        page = await browser.new_page()
-
-        try:
-            await page.goto(LOGIN_URL, timeout=120000)
-
-            await page.fill('input[type="email"]', EMAIL)
-            await page.fill('input[type="password"]', PASSWORD)
-
-            await page.click('button[type="submit"]')
-
-            await page.wait_for_timeout(15000)
-
-            content = await page.content()
-
-            keywords = [
-                "Earliest Available Slot",
-                "Available",
-                "Select Date"
-            ]
-
-            found = any(word in content for word in keywords)
-
-            await send_message(
-    "✅ Telegram test mesajı başarılı. VFS sistemi çalışıyor."
-)
-
-print("TEST MESAJI GONDERILDI")
-
-        except Exception as e:
-            print(e)
-
-        finally:
-            await browser.close()
-
-asyncio.run(check_slots())
+asyncio.run(main())
